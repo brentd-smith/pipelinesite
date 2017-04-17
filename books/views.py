b7cf76e8d3cd9ec344145c38f658a03760cc8622
Brent Smith <bdchan305@gmail.com>
2017-04-17 19:56:34 +0000

    trying to acces env var

--- books/views.py
+++ books/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def index(request):
     return HttpResponse("<h1>Hello World of Django, with Heroku and GitHub Pipelines!! Really complicated.</h1>")
 
 
 def hello(request):
-    return HttpResponse("<h2>Hello</h1>")
+    env = os.environ['APPLICATION_ENVIRONMENT'] 
+    return HttpResponse("<h2>Hello {}</h1>".format(env))
