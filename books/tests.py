4b69160526628de00343efd4ceb18724be1115ab
Brent Smith <bdchan305@gmail.com>
2017-04-18 00:22:56 +0000

    added a single unit test to get started

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+class InitialBooksTests(TestCase):
+
+    def test_hello_world(self):
+        """
+        Hello World of Django Testing
+        """
+        self.assertTrue(True)
+        
